dbdcc5b189eda51ba15b7ab64bf56ff652638bfe
Cj Melendez <melendez.cj@gmail.com>
2017-04-12 17:20:06 -0500

    Did part 4

--- src/test/java/cubesDB.java
+++ src/test/java/cubesDB.java
@@ -1,4 +1,7 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.Scanner;
 
 public class cubesDB {
 
@@ -7,7 +10,43 @@ static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String USER = "cj";
     static final String PASSWORD = "password";
 
-    public static void main(String[] args) {
+
+    public void addEntry (Connection connection) {
+        try {
+            while (true) {
+                ArrayList<String> name = new ArrayList<>();
+                ArrayList<Double> time = new ArrayList<>();
+                String newEntry = getString("Want to add a new entry? Enter 'Y' for yes, 'N' for no.");
+                if (newEntry.equalsIgnoreCase("y")) {
+                    String newName = getString("What was the name of the Cube Solver?");
+                    Double newDouble = getDouble("What was their best time?");
+
+                    name.add(newName);
+                    time.add(newDouble);
+
+                    String prep = "Insert into cube values (? , ?)";
+
+                    PreparedStatement insert = connection.prepareStatement(prep);
+                    insert.setString(1, newName);
+                    insert.setDouble(2, newDouble);
+                    insert.executeUpdate();
+
+                } else if (newEntry.equalsIgnoreCase("n"));
+                {
+                    return;
+                }
+            }
+        } catch (SQLDataException sqle) {
+            System.out.println("Error with the query.");
+            System.out.println(sqle);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        cubesDB cubes = new cubesDB();
 
         try {
             Class.forName(JDBC_DRIVER);
@@ -20,6 +59,11 @@ static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
         try (Connection conn = DriverManager.getConnection(DB_CONNECTION_URL, USER, PASSWORD);
                 Statement statement = conn.createStatement()){
+
+            String refresh = "Drop table cubes";
+            statement.executeUpdate(refresh);
+
+            cubes.addEntry(conn);
 
             String createTableSQL = "CREATE TABLE IF NOT EXISTS cubes (cube_solver varchar(50), how_long double)";
             statement.executeUpdate(createTableSQL);
@@ -47,7 +91,6 @@ static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
             }
 
             String blah = "grant select, insert, update, delete, create, drop on vet.* to 'cj'@'localhost'";
-            
 
             rs.close();
             statement.close();
@@ -58,5 +101,36 @@ static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
         }
 
         System.out.println("Goodbye");
+    }
+
+    public static String getString (String question) {
+        Scanner input = new Scanner(System.in);
+
+        if (question != null){
+            System.out.println(question);
+        }
+        String entry = input.nextLine();
+        return entry;
+    }
+
+    public static Double getDouble (String question) {
+        Scanner inputDouble = new Scanner(System.in);
+
+        if (question != null) {
+            System.out.println(question);
+        }
+        while (true) {
+            try {
+                String stringInput = inputDouble.nextLine();
+                double enterTime = Double.parseDouble(stringInput);
+                if (enterTime >= 0) {
+                    return enterTime;
+                } else {
+                    System.out.println("Please enter positive double");
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please type positive number");
+            }
+        }
     }
 }
